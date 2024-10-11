851a8bdc4d1920057c80858067b8d6ed4743192b
weilin yang <yangw9375@gmail.com>
2024-10-10 19:45:49 -0700

    initial

---
+++ nftbrowser/src/App.js
@@ -0,0 +1,79 @@
+import { Button, Card, Input, Layout, List, message } from "antd";
+import { useState } from "react";
+import { searchNFTs } from "./utils";
+import "./App.css";
+
+const { Header, Content } = Layout;
+
+function App() {
+  const [nfts, setNfts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [searchText, setSearchText] = useState("");
+
+  const handleSearch = async () => {
+    if (searchText === "") {
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+      const data = await searchNFTs(searchText);
+      setNfts(data.result);
+    } catch (error) {
+      message.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <Layout style={{ height: "100vh" }}>
+      <Header>
+        <div style={{ fontSize: 16, fontWeight: 600, color: "white" }}>
+          NFT Browser
+        </div>
+      </Header>
+      <Content
+        style={{ height: "calc(100% - 64px)", padding: 20, overflowY: "auto" }}
+      >
+        <Input.Group compact>
+          <Input
+            style={{ width: 300 }}
+            placeholder="Enter a NFT name to search"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+          <Button type="primary" onClick={handleSearch}>
+            Search
+          </Button>
+        </Input.Group>
+        <List
+          loading={loading}
+          style={{
+            marginTop: 20,
+            height: "calc(100% - 52px)",
+            overflow: "auto",
+          }}
+          grid={{
+            gutter: 16,
+            xs: 1,
+            sm: 3,
+            md: 3,
+            lg: 3,
+            xl: 4,
+            xxl: 4,
+          }}
+          dataSource={[1, 2, 3]}
+          renderItem={(nft) => (
+            <List.Item key={nft}>
+              <Card title={nft} />
+            </List.Item>
+          )}
+        />
+      </Content>
+    </Layout>
+  );
+}
+
+export default App;
---
+++ nftbrowser/src/utils.js
@@ -0,0 +1,50 @@
+const origin = "https://deep-index.moralis.io";
+const apiKey = "APSg0gTbtd4sdazRjKK0p3QBkulqNWGQIy5bVPPL7oLUTJZKh8E265iUncJIIdRg";
+
+// https://docs.moralis.io/reference/nft-api
+export const searchNFTs = async (searchText) => {
+  const url = new URL(`${origin}/api/v2/nft/search`);
+  url.searchParams.append("chain", "eth");
+  url.searchParams.append("format", "decimal");
+  url.searchParams.append("q", searchText);
+  url.searchParams.append("filter", "name");
+  url.searchParams.append("limit", "12");
+
+  const response = await fetch(url, {
+    headers: {
+      accept: "application/json",
+      "X-API-KEY": apiKey,
+    },
+  });
+  return response.json();
+};
+
+export const getNFTTrades = async (tokenAddress) => {
+  const url = new URL(`${origin}/api/v2/nft/${tokenAddress}/trades`);
+  url.searchParams.append("chain", "eth");
+  url.searchParams.append("marketplace", "opensea");
+  url.searchParams.append("limit", "20");
+
+  const response = await fetch(url, {
+    headers: {
+      accept: "application/json",
+      "X-API-KEY": apiKey,
+    },
+  });
+  return response.json();
+};
+
+export const getContractNFTs = async (tokenAddress) => {
+  const url = new URL(`${origin}/api/v2/nft/${tokenAddress}`);
+  url.searchParams.append("chain", "eth");
+  url.searchParams.append("format", "decimal");
+  url.searchParams.append("limit", "20");
+
+  const response = await fetch(url, {
+    headers: {
+      accept: "application/json",
+      "X-API-KEY": apiKey,
+    },
+  });
+  return response.json();
+};
